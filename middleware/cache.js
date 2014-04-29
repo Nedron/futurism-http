e5f86fc3ed0218a323111c42f022f9e62f16f3dc
Jacob Grahn <contact@jiggmin.com>
2014-04-29 01:03:00 -0400

    remove log statement

--- middleware/cache.js
+++ middleware/cache.js
@@ -3,7 +3,6 @@
 module.exports = function(maxAge) {
     
     return function(req, res, next) {
-        console.log('proxyifiyng and reques');
         res.setHeader('Cache-Control', 'public, max-age=' + (maxAge / 1000));
         next();
     }
